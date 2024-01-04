d636cf9a85305e91d1f90e857c55e763d146f75f
NamTaeHo <han127kr@gmail.com>
2024-01-04 19:59:54 +0900

    컴포넌트 스캔 알아보기

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+) //예제 코드 AppConfig 등을 지우지 않고, 사용하기 위해 넣은 것, 원래대로라면 필요 없는 코드이다.
+public class AutoAppConfig {
+
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
